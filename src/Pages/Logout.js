24eee54e1dcb8abceb04a6cf8dadf0d122ffe99c
ultimecreation <naserbechata@gmail.com>
2022-04-22 16:29:52 +0200

    added logout logic

--- src/Pages/Logout.js
+++ src/Pages/Logout.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { connect } from 'react-redux'
+import { Navigate } from 'react-router-dom'
 import Page from '../Layout/Page'
+import { logout } from '../Redux/auth/authActions'
 
-const Logout = () => {
-  return (
-    <Page>
-      Logout
-    </Page>
-  )
+const Logout = ({ logoutUser }) => {
+
+  useEffect(() => {
+    logoutUser()
+  }, [])
+
+  return <Navigate to="/" />
 }
-
-export default Logout
+const mapDispatchToProps = dispatch => {
+  return {
+    logoutUser: () => dispatch(logout())
+  }
+}
+export default connect(null, mapDispatchToProps)(Logout)
